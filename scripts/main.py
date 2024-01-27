c6a68ee85ca21a5999f91f27726e650061c095da
Tal Noznisky <tal.noznisky@gmail.com>
2024-01-27 18:12:54 -0500

    move program into main.py script

---
+++ scripts/main.py
@@ -0,0 +1,38 @@
+import pygame
+import spritesheet
+
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption('tennis ball animation')
+
+BG  = (0, 100, 50)
+sprite_sheet_image = pygame.image.load('./sprites/tennis_ball_sprite.png').convert_alpha()
+
+
+# create multiple sprite sheet objs
+sprites = [spritesheet.SpriteSheet(sprite_sheet_image) for i in range(5)]
+
+
+run = True
+i = 0
+while run:
+    pygame.time.delay(20)
+
+    win.fill(BG)
+    if i < 8:
+        for sprite in sprites:
+            frame = sprite.return_frame(30, 30, 1.5)
+            win.blit(frame, (sprite.x, sprite.y))
+            sprite.move()
+        i += 1
+    if i == 7:
+        i = 0
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+    
+pygame.quit()
